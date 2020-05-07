470ae6ee148003e96d8d44e839512f1c723e3074
PaulRivaud <paulrivaud.info@gmail.com>
2020-05-07 11:40:11 +0200

    improved sle

--- SCRIPTS/sample_list_extraction.py
+++ SCRIPTS/sample_list_extraction.py
@@ -14,11 +14,11 @@ def sle(filename,fromcol='library_id'):
 	fromcol : str
 		Column from which to extract the sample names. Can be `library_id` or `molecule_h5`
 	'''
-    df = pd.read_csv(filename, header=0)
-    if fromcol=='library_id':
-    	return df.library_id.values.tolist()
-    elif fromcol=='molecule_h5':
+	df = pd.read_csv(filename, header=0)
+	if fromcol=='library_id':
+		return df.library_id.values.tolist()
+	elif fromcol=='molecule_h5':
 		return [x.split('/')[-3] for x in df.molecule_h5]
-    else:
-    	raise ValueError('Invalid value for fromcol parameter. Must be one of library_id, molecule_h5')
+	else:
+		raise ValueError('Invalid value for fromcol parameter. Must be one of library_id, molecule_h5')
 
