3208849bb15ede0d3e288fdf9f807dd6ed86fd1a
PaulRivaud <paulrivaud.info@gmail.com>
2020-05-15 18:40:29 +0200

    sample names extraction

--- SCRIPTS/sample_list_extraction.py
+++ SCRIPTS/sample_list_extraction.py
@@ -4,6 +4,7 @@
 # 2020/04
 #############################################################
 
+import os
 import pandas as pd
 
 def sle(filename,fromcol='library_id'):
@@ -16,9 +17,22 @@ def sle(filename,fromcol='library_id'):
 	'''
 	df = pd.read_csv(filename, header=0)
 	if fromcol=='library_id':
-		return df.library_id.values.tolist()
+		return df.library_id.values.tolist() # sample names are the library ids
 	elif fromcol=='molecule_h5':
-		return [x.split('/')[-3] for x in df.molecule_h5]
+		return [x.split('/')[-3] for x in df.molecule_h5] # sample names are the folder names in the aggregation step molecule.h5 paths
 	else:
 		raise ValueError('Invalid value for fromcol parameter. Must be one of library_id, molecule_h5')
 
+def check_samples(L,D):
+	'''
+	Check if samples in `L`exist in `D`
+	Parameters
+	----------
+	L : list
+		List of sample names
+	D : str
+		Directory of individual (unaggregated) samples
+	'''
+	dirlist = next(os.walk(D))[1] # get list of samples (folders) in unaggregated data folder
+	if set(L).issubset(dirlist) == False:
+		raise Exception('Missing some individual samples in %s folder' % D)
