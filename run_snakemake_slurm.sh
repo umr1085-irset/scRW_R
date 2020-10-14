036c873b4aa718d295f7b85fb97154c14cbb9ac2
PaulRivaud <paulrivaud.info@gmail.com>
2020-10-14 11:55:20 +0200

    add slurm script for cluster launch

---
+++ run_snakemake_slurm.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#SBATCH --job-name="snek"
+#SBATCH --output=output_snek.out
+#SBATCH --mem=400G
+#SBATCH --cpus-per-task=50
+#SBATCH --partition=sihp # cluster partition to use
+#SBATCH --mail-user=<user-email> # user email
+#SBATCH --mail-type=ALL # receive emails for all updates
+
+. /local/env/envconda.sh
+conda activate renv # activate R environment
+rm -r .snakemake/locks/ # remove potential locks
+snakemake --dag | dot -Tsvg > OUTPUT/DAG/dag.svg # generate workflow DAG
+snakemake --resources -p -j 48 # launch
